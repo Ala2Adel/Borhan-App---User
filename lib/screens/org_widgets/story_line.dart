9ded60dcea91c6052bfdda3e00541ab845ddc0f2
Mohammed Adel <58287277+mohadel92@users.noreply.github.com>
2020-06-27 04:27:00 +0200

    fixing fluttertoast import

--- lib/screens/org_widgets/story_line.dart
+++ lib/screens/org_widgets/story_line.dart
@@ -2,7 +2,6 @@ import 'package:Borhan_User/models/organization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class Storyline extends StatefulWidget {
   Storyline(this.storyline);
